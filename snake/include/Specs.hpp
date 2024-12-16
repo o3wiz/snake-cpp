5bc33863e0bef4c941b2f60ca5c960246d1bf1d9
Daniel Aviv <dani6laviv@gmail.com>
2024-12-16 15:01:58 +0200

    added include files too, to cmake

--- snake/include/Specs.hpp
+++ snake/include/Specs.hpp
@@ -20,7 +20,7 @@ constexpr Color snakeColor{0x40, 0x5c, 0x00, 0xff};
 constexpr Color snakeTailColor{0x2b, 0x3d, 0x00, 0xff};
 constexpr Color appleColor{0xff, 0x48, 0x24, 0xff};
 constexpr int initialSnakeBodyLength = 3;
-constexpr std::chrono::milliseconds snakeMoveRate{90};
+constexpr std::chrono::milliseconds snakeMoveRate{150};
 constexpr int scoreFontSize = 42;
 constexpr Color scoreColor = DARKGRAY;
 inline const std::filesystem::path resourcesDir = SNAKE_RESOURCES;
